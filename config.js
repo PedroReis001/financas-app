e2a57d24ff3ff40c1377be2db0a6e0098973ca5c
Pedro e Gabriel <pedroegabriel@MacBook-Air-Preto.local>
2026-06-21 18:39:45 -0300

    fix: espaço na URL do Supabase e telas sobrepostas

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
 // aqui.
 
 window.APP_CONFIG = {
-  SUPABASE_URL:" https://uxbswqguwcsbolwaewbe.supabase.co",
+  SUPABASE_URL: "https://uxbswqguwcsbolwaewbe.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YnN3cWd1d2NzYm9sd2Fld2JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5OTY1NzUsImV4cCI6MjA5NzU3MjU3NX0.ayJcXDdC63I6S_bjXzsLC7NqkzD3x3bzDLpLKjW9ilU",
 };
